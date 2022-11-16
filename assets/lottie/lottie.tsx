fbb748a1de7f207bdf0712d68f3276d09ff9c664
shuvamk <kshuvam564@gmail.com>
2022-11-16 20:31:26 +0530

    Add new lottie for lessons

--- assets/lottie/lottie.tsx
+++ assets/lottie/lottie.tsx
@@ -1,6 +1,25 @@
 import Lesson1Lottie from "./chill.json";
-import Lesson2Lottie from "./think.json";
+import Lesson2Lottie from "./coding.json";
 import Lesson3Lottie from "./calm.json";
 import Lesson4Lottie from "./relax.json";
+import Lesson5Lottie from "./meditate.json";
+import Lesson6Lottie from "./meditation2.json";
+import Lesson7Lottie from "./think.json";
+import Lesson8Lottie from "./multiple-hands.json";
+import Lesson9Lottie from "./doubt.json";
+import Lesson10Lottie from "./chats.json";
+import Lesso11Lottie from "./cap.json";
 
-export { Lesson1Lottie, Lesson2Lottie, Lesson3Lottie, Lesson4Lottie };
+export {
+  Lesson1Lottie,
+  Lesson2Lottie,
+  Lesson3Lottie,
+  Lesson4Lottie,
+  Lesson5Lottie,
+  Lesson6Lottie,
+  Lesson7Lottie,
+  Lesson8Lottie,
+  Lesson9Lottie,
+  Lesson10Lottie,
+  Lesso11Lottie,
+};
