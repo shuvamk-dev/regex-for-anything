791630c761a634a928bd66ddbd8492081c2e471f
shuvamk <kshuvam564@gmail.com>
2022-11-16 17:03:20 +0530

    Add lottie on top

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import Lottie from "lottie-react";
 import Link from "next/link";
 import React from "react";
 import AutoComplete from "../components/AutoComplete";
 import Button from "../components/Button";
+
+import EmptyStateLottie from "../assets/lottie/emptyState.json";
 
 import styles from "./index.module.css";
 
@@ -9,7 +12,16 @@ const Home = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.topSection}>
-        <div className={styles.heading}>RegEX.</div>
+        <div className={styles.heading}>
+          <Lottie
+            animationData={EmptyStateLottie}
+            style={{ height: "160px" }}
+            loop={true}
+            className={styles.lottie}
+          />
+          RegEX.
+        </div>
+
         <div className={styles.subHeading}>
           Quickly find most used Regular Expressions.
         </div>
