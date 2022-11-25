0829f11fada0210a890e402d61a44eb284e9cf33
shuvamk <kshuvam564@gmail.com>
2022-11-25 15:37:15 +0530

    Remove unused lottie files

--- assets/lottie/lottie.tsx
+++ assets/lottie/lottie.tsx
@@ -2,24 +2,5 @@ import IntroWarrior from "./intro_warrior.json";
 import keyLottie from "./key.json";
 import potionLottie from "./potion.json";
 import avocadoLottie from "./avocado.json";
-import Lesson5Lottie from "./meditate.json";
-import Lesson6Lottie from "./meditation2.json";
-import Lesson7Lottie from "./think.json";
-import Lesson8Lottie from "./multiple-hands.json";
-import Lesson9Lottie from "./doubt.json";
-import Lesson10Lottie from "./chats.json";
-import Lesso11Lottie from "./cap.json";
 
-export {
-  IntroWarrior,
-  keyLottie,
-  potionLottie,
-  avocadoLottie,
-  Lesson5Lottie,
-  Lesson6Lottie,
-  Lesson7Lottie,
-  Lesson8Lottie,
-  Lesson9Lottie,
-  Lesson10Lottie,
-  Lesso11Lottie,
-};
+export { IntroWarrior, keyLottie, potionLottie, avocadoLottie };
